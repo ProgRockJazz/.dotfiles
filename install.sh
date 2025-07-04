4ac11f747e6d4566f5922b40d0f6b53afd8d4d5d
paxman101 <paxmanrocks@gmail.com>
2025-07-04 11:58:38 -0700

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
+APT_PACKAGES="neovim"
 if command -v apt &>/dev/null; then
-	sudo apt update && sudo apt install -y
+	sudo apt update && sudo apt install -y $APT_PACKAGES
 fi
 # Install oh my posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
