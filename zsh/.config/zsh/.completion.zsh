f094cce3e2571f6e5fede2a648f0de3a10c1c51e
Camille Schweigert <camille.schweigert@credosemi.com>
2026-05-27 17:59:00 -0700

    update nvim + completions

--- zsh/.config/zsh/.completion.zsh
+++ zsh/.config/zsh/.completion.zsh
@@ -105,3 +105,32 @@ zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f
 #source $DOTFILES/zsh/plugins/kubectl-completion/_kubectl
 #zstyle ':completion:*:*:kubectl:*' list-grouped false
 
+
+#compdef opencode
+###-begin-opencode-completions-###
+#
+# yargs command completion script
+#
+# Installation: opencode completion >> ~/.zshrc
+#    or opencode completion >> ~/.zprofile on OSX.
+#
+_opencode_yargs_completions()
+{
+  local reply
+  local si=$IFS
+  IFS=$'
+' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" opencode --get-yargs-completions "${words[@]}"))
+  IFS=$si
+  if [[ ${#reply} -gt 0 ]]; then
+    _describe 'values' reply
+  else
+    _default
+  fi
+}
+if [[ "'${zsh_eval_context[-1]}" == "loadautofunc" ]]; then
+  _opencode_yargs_completions "$@"
+else
+  compdef _opencode_yargs_completions opencode
+fi
+###-end-opencode-completions-####
+
